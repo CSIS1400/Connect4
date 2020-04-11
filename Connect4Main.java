114cda9152d33d201bf899b10a8113731484ab68
freerunn14 <62972509+freerunn14@users.noreply.github.com>
2020-04-10 19:41:44 -0600

    Update Connect4Main.java

--- Connect4Main.java
+++ Connect4Main.java
@@ -1,4 +1,15 @@
-public class Connect4Main{
+public class AttemptConnect4 {
+
+   public static void main (String[] args) {
+         
+      ConnectBrain CB = new ConnectBrain();
+      int num = 0;
+      do{
+         CB.printBoard();
+         CB.columnSelector();
+         
+      
+   }while(num == 0);
    
-   
+   }
 }

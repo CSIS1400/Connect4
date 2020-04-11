1a9d2567b3ad498dcb24c547a4cff26c75b6b16f
freerunn14 <62972509+freerunn14@users.noreply.github.com>
2020-04-10 19:43:02 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,40 @@
-public class Player{
+import java.util.Scanner;
+
+public class ConnectPlayer {
+
+   static Scanner input = new Scanner(System.in);
+   static int decider = 0;
+   char player = 'R';
    
+   public int playerSelection() {
+      if(player == 'R'){
+         System.out.println("*** Player 1's Turn ***");
+         System.out.print("Enter a column number 1-7: ");
+      }else{
+         System.out.println("*** Player 2's Turn ***");
+         System.out.print("Enter a column number 1-7: ");
+      }
+      int selection = input.nextInt();
+      System.out.println();
+      while(selection <= 0 || selection >= 8){
+         System.out.print("Please Enter A Valid Number 1-9: ");
+         selection = input.nextInt();
+      }
+      return selection;    
+   }
    
+   public int decider() {
+      decider = decider + 1;
+      if(decider % 2 == 1){
+         player = 'R';
+      }else{
+         player = 'Y';
+      }
+      return player;
+   }
+   
+      
+      
+
+
 }

509081263acbbfafd7a0229eefc4b6dfa5e76e7e
calebmcfalls13 <62972190+calebmcfalls13@users.noreply.github.com>
2020-04-16 15:31:33 -0600

    fixed

--- Board.java
+++ Board.java
@@ -12,7 +12,7 @@ public class Board{
    
    // print method
    public void printBoard(){
-      System.out.printf("| 1 | 2 | 3 | 4 | 5 | 6 | 7 |%n");
+      System.out.printf("  1   2   3   4   5   6   7  %n");
       System.out.printf("| %c | %c | %c | %c | %c | %c | %c | ",boardArray[0][0],boardArray[0][1],boardArray[0][2],
          boardArray[0][3],boardArray[0][4],boardArray[0][5],boardArray[0][6]);
    }
